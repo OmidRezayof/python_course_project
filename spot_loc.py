88f70a3f2bf65f65764988b3a74ff9a4fdd6226d
OmidRezayof <143108739+OmidRezayof@users.noreply.github.com>
2023-11-24 14:41:09 -0600

    Update spot_loc.py

--- spot_loc.py
+++ spot_loc.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Point
 
 spot_loc = [0, 0]
-obj_loc = [0,10] # should be fed with the sensor. for now, I'm reading it with subscription
+obj_loc = [0,20] # should be fed with the sensor. for now, I'm reading it with subscription
 goal_loc = [0, 200]
 visibility_threshold=50
 
@@ -32,7 +32,7 @@ def pos_process():
     publishing_msg = Point()
     global spot_loc, obj_loc, goal_loc
     
-    robot_object_distance = 10
+    robot_object_distance = 20
     
     goal_obj_dist=((obj_loc[0] - goal_loc[0])**2+(obj_loc[1] - goal_loc[1])**2)**0.5
     spot_obj_dist=((obj_loc[0] - spot_loc[0])**2+(spot_loc[1] - obj_loc[1])**2)**0.5
