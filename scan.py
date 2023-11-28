e752d198c3f07f2b2f837e4c6ce2eb87349029e0
jives01 <85960552+jives01@users.noreply.github.com>
2023-11-27 18:36:50 -0600

    Update scan.py

--- scan.py
+++ scan.py
@@ -2,27 +2,25 @@
 import math
 import rospy
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Point
-from numpy import array,inf
+from geometry_msg.msg import Point
 
 def locations(msg):
     #whatever
     #msg.ranges is the ranges array
-    dists = list(msg.ranges)
+    dists = msg.ranges
     # depth = 5 #arbitrary value, may need to find the depth of the husky based on range readings
     min_ang = msg.angle_min
     ang_inc = msg.angle_increment
     publishing_msg = Point()
     sensor_pubmsg=Point()
 
-    for i in range(len(dists)):
-    
-        if dists[i] == inf or dists[i] == -inf:
-            dists[i] = 0 
-            
+    # for i in range(len(dists)):
+    #     if dists[i] == 'inf':
+    #         dists[i] = 0 
     #need to get rid of the inf values to be able to process data
     
-    obj_dist = min(value for value in dists if value != 0)
+    # obj_dist = min(value for value in dists if value != 0)
+    obj_dist = min(value for value in dists)
     obj_ind = dists.index(obj_dist)
     obj_ang = min_ang + ang_inc*obj_ind  #should be object angle in radians
     obj_x = obj_dist*math.cos(obj_ang)
@@ -30,20 +28,26 @@ def locations(msg):
     obj_loc = [obj_x, obj_y] 
     #obj_loc should have the x, y coordinates of the object with respect to the lidar
     #may not need goal_loc if we use the joystick to control the goal
-    
-    goal_dist = max(dists)
-    goal_ind = dists.index(goal_dist)
-    goal_angle = min_ang + ang_inc*goal_ind
-    x_s = goal_dist*math.cos(goal_angle)
-    y_s = goal_dist*math.sin(goal_angle)
-
+    n = 0 
+    goal = [[0,0,[0,0]]]
+    while n < 2: #going to find 2 max distances to use as goal posts
+        dist = max(dists)
+        ind = dists.index(dist)
+        angle = min_ang + ang_inc*ind
+        x = dist*math.cos(angle)
+        y = dist*math.sin(angle)
+        dists[ind] = 0
+        goal[n] = [x,y]
+        n = n+1
+    x_s = (goal[0][0]+goal[1][0])/2
+    y_s = (goal[0][1]+goal[1][1])/2
     goal_loc = [x_s,y_s]
     publishing_msg.x=obj_x
     publishing_msg.y=obj_y
-    publishing_msg.z = obj_dist
+    publishing_msg.z = 0
     sensor_pubmsg.x=x_s
     sensor_pubmsg.y=y_s
-    sensor_pubmsg.z=((x_s-obj_x)**2+(y_s-obj_y)**2)**0.5
+    sensor_pubmsg.z=0
     if not rospy.is_shutdown():
         pub.publish(publishing_msg)
         pub2.publish(sensor_pubmsg)
